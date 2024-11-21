fb4b85d66f16e73a0c03b81fe4ed352e72f2520f
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-11-21 09:13:42 +0100

    setup as a library crate

---
+++ src/drone.rs
@@ -0,0 +1,23 @@
+use crossbeam_channel::{Receiver, Sender};
+use wg_2024::controller::Command;
+use wg_2024::drone::{Drone, DroneOptions};
+use wg_2024::network::NodeId;
+use wg_2024::packet::Packet;
+
+struct RustBusterDrone {
+    id: NodeId,
+    sim_contr_send: Sender<Command>,
+    sim_contr_recv: Receiver<Command>,
+    packet_recv: Receiver<Packet>,
+    pdr: f32,
+}
+
+impl Drone for RustBusterDrone {
+    fn new(options: DroneOptions) -> Self {
+        todo!()
+    }
+
+    fn run(&mut self) {
+        todo!()
+    }
+}
