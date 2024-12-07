16e1995da7e774bb49c2c0c43976a813816e54c4
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-12-07 10:31:22 +0100

    some flooding unit tests

--- src/drone/handle_flood.rs
+++ src/drone/handle_flood.rs
@@ -83,7 +83,7 @@ impl RustBustersDrone {
             session_id,
         };
         if let Some(sender) = self.packet_send.get(&sender_id) {
-            if let Err(e) = sender.send(response_packet) {
+            if let Err(e) = sender.send(response_packet.clone()) {
                 self.packet_send.remove(&sender_id);
                 error!(
                     "Drone {}: Error sending FloodResponse({}) to {}: {}",
@@ -93,6 +93,8 @@ impl RustBustersDrone {
                     "Drone {}: Neighbor {} has been removed from packet_send due to channel closure",
                     self.id, sender_id
                 );
+
+                self.send_packet_to_sc(response_packet);
             } else {
                 info!(
                     "Drone {}: FloodResponse({}) sent to {}",
@@ -104,6 +106,8 @@ impl RustBustersDrone {
                 "Drone {}: Sender {} not found in packet_send.",
                 self.id, sender_id
             );
+
+            self.send_packet_to_sc(response_packet);
         }
     }
 
